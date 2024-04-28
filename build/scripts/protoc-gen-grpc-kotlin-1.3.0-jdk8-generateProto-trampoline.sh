fd085555081c8c290812c598aa4c76e1858e6a05
Mulicheva <miokeit@gmail.com>
2024-04-29 01:04:19 +0300

    TESTS for grpc api

---
+++ build/scripts/protoc-gen-grpc-kotlin-1.3.0-jdk8-generateProto-trampoline.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+exec '/Library/Java/JavaVirtualMachines/jdk-11.0.13.jdk/Contents/Home/bin/java' -jar '/Users/admin/.gradle/caches/modules-2/files-2.1/io.grpc/protoc-gen-grpc-kotlin/1.3.0/cb20cb447ec7c77d59d118580db3630c92b9b0bb/protoc-gen-grpc-kotlin-1.3.0-jdk8.jar' "$@"
